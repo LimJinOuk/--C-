095eb56c72270d9458bc725c77833350f559b713
LimJinOuk <105255163+LimJinOuk@users.noreply.github.com>
2022-09-10 11:36:31 +0900

    Delete 연습과제 directory

--- 연습과제/1주차/01번/1주차 연습문제 01.C
+++
@@ -1,6 +0,0 @@
-﻿#include <stdio.h>
-
-int main(void) {
-	printf("홍길동\n홍 길 동\n홍  길  동");
-	return 0;
-}
--- 연습과제/1주차/02번/1주차 연습과제 02.C
+++
@@ -1,8 +0,0 @@
-﻿#include <stdio.h>
-
-int main(void) {
-	printf("나의 이름은 : 임진욱\n");
-	printf("나의 주소 : 중계로8길 39 현대 5차 @\n");
-	printf("나의 번호는 : 010 2214 6532\n");
-	return 0;
-}
