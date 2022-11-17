89e590859cc29263b5cbe1f157666b108b76aa0b
LimJinOuk <105255163+LimJinOuk@users.noreply.github.com>
2022-11-17 13:36:33 +0900

    Add files via upload

--- hw8.c
+++ hw8.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 #include<math.h>
 
-int arr[5];
-int SD;
-int* ptr_arr;
-int i;
-int num1, num2, num3, num4, num5 , SD , Mean;
-int main(void) {
-	ptr_arr = &arr;
-	/*
-	for (i = 0; i < 5; i++) {
-		printf("Enter 5 real numbers.:");
-		scanf_s("%d", &arr[i]);
-	}
-	*/
+float arr[5];
+float SD;
+float* ptr_arr = &arr;
+float num1, num2, num3, num4, num5 , SD , Mean;
 
-	/*
-	printf("Enter 5 real numbers.:");
-	scanf_s("%s", arr, sizeof(arr));
-	*/
+float sd(float *ptr_arr) {
+	float n1 = *ptr_arr;
+	float n2 = *(ptr_arr + 1);
+	float n3 = *(ptr_arr + 2);
+	float n4 = *(ptr_arr + 3);
+	float n5 = *(ptr_arr + 4);
+	float mean = (n1 + n2 + n3 + n4 + n5) / 5.0;
 
-	printf("%s\n", arr);
+	float pow_n1 = pow((n1 - mean), 2);
+	float pow_n2 = pow((n2 - mean), 2);
+	float pow_n3 = pow((n3 - mean), 2);
+	float pow_n4 = pow((n4 - mean), 2);
+	float pow_n5 = pow((n5 - mean), 2);
+	
+	float temp = pow_n1 + pow_n2 + pow_n3 + pow_n4 + pow_n5;
 
-	num1 = arr[0];
-	num2 = arr[1];
-	num3 = arr[2];
-	num4 = arr[3];
-	num5 = arr[4];
-
-	printf("%d\n%d\n%d\n%d%\n%d\n", num1, num2, num3, num4, num5);
-	printf("%d", ptr_arr);
+	float sd = sqrt((temp / 5));
+	
+	return sd;
 }
+
+int main(void) {
+
+	printf("Enter 5 real numbers.:");
+	scanf_s("%f %f %f %f %f", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
+	printf("Standard Deviation = %f", sd(ptr_arr));
+
+	return 0;
+}
+
+
+
